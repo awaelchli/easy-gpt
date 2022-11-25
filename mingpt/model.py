cf141e8ba56d5ae4ecb28afc2de2d4e09d047907
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-11-26 00:38:44 +0100

    debug

--- mingpt/model.py
+++ mingpt/model.py
@@ -8,8 +8,8 @@ https://github.com/openai/gpt-2/blob/master/src/model.py
 https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
 """
 
-from dataclasses import dataclass
 import math
+from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
@@ -21,8 +21,19 @@ class NewGELU(nn.Module):
     Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
     Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
     """
+
     def forward(self, x):
-        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+        return (
+            0.5
+            * x
+            * (
+                1.0
+                + torch.tanh(
+                    math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))
+                )
+            )
+        )
+
 
 class CausalSelfAttention(nn.Module):
     """
@@ -42,80 +53,105 @@ class CausalSelfAttention(nn.Module):
         self.attn_dropout = nn.Dropout(config.attn_pdrop)
         self.resid_dropout = nn.Dropout(config.resid_pdrop)
         # causal mask to ensure that attention is only applied to the left in the input sequence
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                     .view(1, 1, config.block_size, config.block_size))
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
         self.n_head = config.n_head
         self.n_embd = config.n_embd
 
     def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        (
+            B,
+            T,
+            C,
+        ) = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = (
+            y.transpose(1, 2).contiguous().view(B, T, C)
+        )  # re-assemble all head outputs side by side
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
         return y
 
+
 class Block(nn.Module):
-    """ an unassuming Transformer block """
+    """an unassuming Transformer block"""
 
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
-        self.mlp = nn.ModuleDict(dict(
-            c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd),
-            c_proj  = nn.Linear(4 * config.n_embd, config.n_embd),
-            act     = NewGELU(),
-            dropout = nn.Dropout(config.resid_pdrop),
-        ))
+        self.mlp = nn.ModuleDict(
+            dict(
+                c_fc=nn.Linear(config.n_embd, 4 * config.n_embd),
+                c_proj=nn.Linear(4 * config.n_embd, config.n_embd),
+                act=NewGELU(),
+                dropout=nn.Dropout(config.resid_pdrop),
+            )
+        )
         m = self.mlp
-        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x)))) # MLP forward
+        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))  # MLP forward
 
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlpf(self.ln_2(x))
         return x
 
+
 class GPT(nn.Module):
-    """ GPT Language Model """
+    """GPT Language Model"""
 
     def __init__(self, config):
         super().__init__()
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.block_size = config.block_size
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
-            drop = nn.Dropout(config.embd_pdrop),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = nn.LayerNorm(config.n_embd),
-        ))
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
+                drop=nn.Dropout(config.embd_pdrop),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f=nn.LayerNorm(config.n_embd),
+            )
+        )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
         self.apply(self._init_weights)
         for pn, p in self.named_parameters():
-            if pn.endswith('c_proj.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer)
+                )
 
-        # report number of parameters (note we don't count the decoder parameters in lm_head)
-        n_params = sum(p.numel() for p in self.transformer.parameters())
-        print("number of parameters: %.2fM" % (n_params/1e6,))
+    @property
+    def num_parameters(self):
+        # note: we don't count the decoder parameters in lm_head
+        return sum(p.numel() for p in self.transformer.parameters())
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -128,7 +164,6 @@ class GPT(nn.Module):
             torch.nn.init.zeros_(module.bias)
             torch.nn.init.ones_(module.weight)
 
-
     def configure_optimizers(self, train_config):
         """
         This long function is unfortunately doing something very simple and is being very defensive:
@@ -140,21 +175,21 @@ class GPT(nn.Module):
         # separate out all parameters to those that will and won't experience regularizing weight decay
         decay = set()
         no_decay = set()
-        whitelist_weight_modules = (torch.nn.Linear, )
+        whitelist_weight_modules = (torch.nn.Linear,)
         blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
         for mn, m in self.named_modules():
             for pn, p in m.named_parameters():
-                fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+                fpn = "%s.%s" % (mn, pn) if mn else pn  # full param name
                 # random note: because named_modules and named_parameters are recursive
                 # we will see the same tensors p many many times. but doing it this way
                 # allows us to know which parent module any tensor p belongs to...
-                if pn.endswith('bias'):
+                if pn.endswith("bias"):
                     # all biases will not be decayed
                     no_decay.add(fpn)
-                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                elif pn.endswith("weight") and isinstance(m, whitelist_weight_modules):
                     # weights of whitelist modules will be weight decayed
                     decay.add(fpn)
-                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                elif pn.endswith("weight") and isinstance(m, blacklist_weight_modules):
                     # weights of blacklist modules will NOT be weight decayed
                     no_decay.add(fpn)
 
@@ -162,27 +197,46 @@ class GPT(nn.Module):
         param_dict = {pn: p for pn, p in self.named_parameters()}
         inter_params = decay & no_decay
         union_params = decay | no_decay
-        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
-        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
-                                                    % (str(param_dict.keys() - union_params), )
+        assert (
+            len(inter_params) == 0
+        ), "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
+        assert (
+            len(param_dict.keys() - union_params) == 0
+        ), "parameters %s were not separated into either decay/no_decay set!" % (
+            str(param_dict.keys() - union_params),
+        )
 
         # create the pytorch optimizer object
         optim_groups = [
-            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.weight_decay},
-            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+            {
+                "params": [param_dict[pn] for pn in sorted(list(decay))],
+                "weight_decay": train_config.weight_decay,
+            },
+            {
+                "params": [param_dict[pn] for pn in sorted(list(no_decay))],
+                "weight_decay": 0.0,
+            },
         ]
-        optimizer = torch.optim.AdamW(optim_groups, lr=train_config.learning_rate, betas=train_config.betas)
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=train_config.learning_rate, betas=train_config.betas
+        )
         return optimizer
 
     def forward(self, idx, targets=None):
         device = idx.device
         b, t = idx.size()
-        assert t <= self.block_size, f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
-        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t)
+        assert (
+            t <= self.block_size
+        ), f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(
+            0
+        )  # shape (1, t)
 
         # forward the GPT model itself
-        tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
-        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (1, t, n_embd)
+        tok_emb = self.transformer.wte(idx)  # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(
+            pos
+        )  # position embeddings of shape (1, t, n_embd)
         x = self.transformer.drop(tok_emb + pos_emb)
         for block in self.transformer.h:
             x = block(x)
@@ -192,12 +246,16 @@ class GPT(nn.Module):
         # if we are given some desired targets also calculate the loss
         loss = None
         if targets is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
 
         return logits, loss
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
+    def generate(
+        self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None
+    ):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
@@ -205,7 +263,9 @@ class GPT(nn.Module):
         """
         for _ in range(max_new_tokens):
             # if the sequence context is growing too long we must crop it at block_size
-            idx_cond = idx if idx.size(1) <= self.block_size else idx[:, -self.block_size:]
+            idx_cond = (
+                idx if idx.size(1) <= self.block_size else idx[:, -self.block_size :]
+            )
             # forward the model to get the logits for the index in the sequence
             logits, _ = self(idx_cond)
             # pluck the logits at the final step and scale by desired temperature
@@ -213,7 +273,7 @@ class GPT(nn.Module):
             # optionally crop the logits to only the top k options
             if top_k is not None:
                 v, _ = torch.topk(logits, top_k)
-                logits[logits < v[:, [-1]]] = -float('Inf')
+                logits[logits < v[:, [-1]]] = -float("Inf")
             # apply softmax to convert logits to (normalized) probabilities
             probs = F.softmax(logits, dim=-1)
             # either sample from the distribution or take the most likely element
--- projects/chargpt/chargpt.py
+++ projects/chargpt/chargpt.py
@@ -1,58 +1,59 @@
 """
 Trains a character-level language model.
 """
-from dataclasses import dataclass
+import functools
 import time
+from dataclasses import dataclass
 from typing import Optional, Tuple
+
 import torch
+from lightning_lite import seed_everything
+from lightning_lite.lite import LightningLite
+from lightning_lite.strategies.fsdp import FSDPStrategy
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    CheckpointImpl, apply_activation_checkpointing, checkpoint_wrapper)
+from torch.distributed.fsdp import BackwardPrefetch, CPUOffload
+from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 
 from mingpt.model import GPT, Block
-import functools
-from lightning_lite import seed_everything
-from lightning_lite.lite import LightningLite
-from lightning_lite.strategies.fsdp import FSDPStrategy
-from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
-from torch.distributed.fsdp import CPUOffload, BackwardPrefetch
-from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
-    checkpoint_wrapper,
-    CheckpointImpl,
-    apply_activation_checkpointing
-)
+
 
 @dataclass
 class GPTConfig:
     model_type: str
     vocab_size: int
     block_size: int
-    embd_pdrop: float =  0.1
-    resid_pdrop: float =  0.1
-    attn_pdrop: float =  0.1
+    embd_pdrop: float = 0.1
+    resid_pdrop: float = 0.1
+    attn_pdrop: float = 0.1
     n_layer: Optional[int] = None
     n_head: Optional[int] = None
-    n_embd:  Optional[int] = None
+    n_embd: Optional[int] = None
 
     def __post_init__(self):
         type_given = self.model_type is not None
-        params_given = all((self.n_layer is not None, self.n_head is not None, self.n_embd is not None))
+        params_given = all(
+            (self.n_layer is not None, self.n_head is not None, self.n_embd is not None)
+        )
         assert type_given ^ params_given
         if type_given:
             # translate from model_type to detailed configuration
             values = {
                 # names follow the huggingface naming conventions
                 # GPT-1
-                'openai-gpt':   dict(n_layer=12, n_head=12, n_embd=768),  # 117M params
+                "openai-gpt": dict(n_layer=12, n_head=12, n_embd=768),  # 117M params
                 # GPT-2 configs
-                'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-                'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-                'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-                'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
-                'gpt2-xxl':      dict(n_layer=15, n_head=16, n_embd=3072), # ...
+                "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+                "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+                "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+                "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
             }[self.model_type]
-            self.n_layer=values["n_layer"]
-            self.n_head=values["n_head"]
-            self.n_embd=values["n_embd"]
+            self.n_layer = values["n_layer"]
+            self.n_head = values["n_head"]
+            self.n_embd = values["n_embd"]
+
 
 @dataclass
 class TrainerConfig:
@@ -67,40 +68,34 @@ class TrainerConfig:
     max_iters: int = -1
 
 
-
-
 model_config = GPTConfig(
-    model_type = 'gpt2-xxl',
-    vocab_size = None,
-    block_size =  128,
-    embd_pdrop = 0.1,
-    resid_pdrop = 0.1,
-    attn_pdrop = 0.1,
+    model_type="gpt2-xl",
+    vocab_size=None,
+    block_size=128,
+    embd_pdrop=0.1,
+    resid_pdrop=0.1,
+    attn_pdrop=0.1,
 )
 
 
 trainer_config = TrainerConfig(
-    num_workers = 4,
-    max_iters = 100,
-    block_size = 128,
-    batch_size = 1,
-    learning_rate = 3e-4,
-    betas = (0.9, 0.95),
-    weight_decay = 0.1, # only applied on matmul weights
-    grad_norm_clip = 1.0,
+    num_workers=4,
+    max_iters=100,
+    block_size=128,
+    batch_size=1,
+    learning_rate=3e-4,
+    betas=(0.9, 0.95),
+    weight_decay=0.1,  # only applied on matmul weights
+    grad_norm_clip=1.0,
 )
 
 
 class CharDataset(Dataset):
-    """
-    Emits batches of characters
-    """
-
     def __init__(self, data, block_size):
         self.block_size = block_size
         chars = sorted(list(set(data)))
-        self.stoi = { ch:i for i,ch in enumerate(chars) }
-        self.itos = { i:ch for i,ch in enumerate(chars) }
+        self.stoi = {ch: i for i, ch in enumerate(chars)}
+        self.itos = {i: ch for i, ch in enumerate(chars)}
         self.vocab_size = len(chars)
         self.data = data
 
@@ -115,7 +110,7 @@ class CharDataset(Dataset):
 
     def __getitem__(self, idx):
         # grab a chunk of (block_size + 1) characters from the data
-        chunk = self.data[idx:idx + self.block_size + 1]
+        chunk = self.data[idx : idx + self.block_size + 1]
         # encode every character to an integer
         dix = [self.stoi[s] for s in chunk]
         # return as tensors
@@ -127,17 +122,32 @@ class CharDataset(Dataset):
 def main():
     seed_everything(trainer_config.seed)
 
-    auto_wrap_policy = functools.partial(transformer_auto_wrap_policy, transformer_layer_cls={Block})
+    auto_wrap_policy = functools.partial(
+        transformer_auto_wrap_policy, transformer_layer_cls={Block}
+    )
     check_fn = lambda submodule: isinstance(submodule, Block)
-    wrapper = functools.partial(checkpoint_wrapper, offload_to_cpu=False, checkpoint_impl=CheckpointImpl.NO_REENTRANT)
+    wrapper = functools.partial(
+        checkpoint_wrapper,
+        offload_to_cpu=False,
+        checkpoint_impl=CheckpointImpl.NO_REENTRANT,
+    )
 
     # TODO: precision 16 and cpu offload hangs
-    # TODO: error messaging for cpu-offload + wrap policy
-    lite = LightningLite(accelerator="cuda", devices=4, precision=16, strategy=FSDPStrategy(auto_wrap_policy=auto_wrap_policy, backward_prefetch=BackwardPrefetch.BACKWARD_PRE))
+    lite = LightningLite(
+        accelerator="cuda",
+        devices=4,
+        precision=16,
+        strategy=FSDPStrategy(
+            auto_wrap_policy=auto_wrap_policy,
+            backward_prefetch=BackwardPrefetch.BACKWARD_PRE,
+        ),
+    )
     lite.launch()
 
     # construct the training dataset
-    text = open('data/tinyshakespeare.txt', 'r').read() # don't worry we won't run out of file handles
+    text = open(
+        "data/tinyshakespeare.txt", "r"
+    ).read()  # don't worry we won't run out of file handles
     train_dataset = CharDataset(text, block_size=model_config.block_size)
 
     # construct the model
@@ -151,15 +161,19 @@ def main():
         model = GPT(model_config)
     model = lite.setup_module(model)
 
-    apply_activation_checkpointing(model, checkpoint_wrapper_fn=wrapper, check_fn=check_fn)
+    lite.print(f"Number of parameters: {model.num_parameters() / 1e6:.1f} M")
+
+    apply_activation_checkpointing(
+        model, checkpoint_wrapper_fn=wrapper, check_fn=check_fn
+    )
 
     # TODO: support multiple param groups for FSDP
     # optimizer = model.configure_optimizers(config.trainer)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=trainer_config.learning_rate, betas=trainer_config.betas)
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=trainer_config.learning_rate, betas=trainer_config.betas
+    )
     optimizer = lite.setup_optimizers(optimizer)
 
-
-    # setup the dataloader
     train_loader = DataLoader(
         train_dataset,
         # TODO: fix this in Lite
@@ -169,11 +183,10 @@ def main():
         batch_size=trainer_config.batch_size,
         num_workers=trainer_config.num_workers,
     )
-
     train_loader = lite.setup_dataloaders(train_loader)
 
     model.train()
-    iter_num = 0
+    iteration = 0
     iter_dt = 0
     iter_time = time.time()
     data_iter = iter(train_loader)
@@ -187,30 +200,30 @@ def main():
 
         x, y = batch
 
-        # forward the model
-        logits, loss = model(x, y)
-
-        # backprop and update the parameters
+        _, loss = model(x, y)
         model.zero_grad(set_to_none=True)
         lite.backward(loss)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), trainer_config.grad_norm_clip)
+        torch.nn.utils.clip_grad_norm_(
+            model.parameters(), trainer_config.grad_norm_clip
+        )
         optimizer.step()
 
-        if iter_num % 10 == 0:
-            lite.print(f"iter_dt {iter_dt * 1000:.2f}ms; iter {iter_num}: train loss {loss.item():.5f}")
+        if iteration % 10 == 0:
+            lite.print(
+                f"iteration time {iter_dt * 1000:.2f}ms; iteration {iteration}; train loss {loss.item():.5f}"
+            )
 
-        iter_num += 1
+        iteration += 1
         tnow = time.time()
         iter_dt = tnow - iter_time
         iter_time = tnow
 
-        # termination conditions
-        if trainer_config.max_iters != -1 and iter_num >= trainer_config.max_iters:
+        if trainer_config.max_iters != -1 and iteration >= trainer_config.max_iters:
             break
 
     # For optimal memory throughput, make sure the summary shows 0 cudaMalloc retries and otherwise try lowering the batch size.
-    print(torch.cuda.memory_summary())
+    lite.print(torch.cuda.memory_summary())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
