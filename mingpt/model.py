799ba1345877f373eb8f0c49af7ddc0051eaf83a
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-11-26 01:10:09 +0100

    Refactor to Lightning Lite (#2)

--- mingpt/model.py
+++ mingpt/model.py
@@ -9,14 +9,11 @@ https://github.com/huggingface/transformers/blob/main/src/transformers/models/gp
 """
 
 import math
+from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
-from mingpt.utils import CfgNode as CN
-
-# -----------------------------------------------------------------------------
 
 
 class NewGELU(nn.Module):
@@ -127,66 +124,11 @@ class Block(nn.Module):
 class GPT(nn.Module):
     """GPT Language Model"""
 
-    @staticmethod
-    def get_default_config():
-        C = CN()
-        # either model_type or (n_layer, n_head, n_embd) must be given in the config
-        C.model_type = "gpt"
-        C.n_layer = None
-        C.n_head = None
-        C.n_embd = None
-        # these options must be filled in externally
-        C.vocab_size = None
-        C.block_size = None
-        # dropout hyperparameters
-        C.embd_pdrop = 0.1
-        C.resid_pdrop = 0.1
-        C.attn_pdrop = 0.1
-        return C
-
     def __init__(self, config):
         super().__init__()
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.block_size = config.block_size
-
-        type_given = config.model_type is not None
-        params_given = all(
-            [
-                config.n_layer is not None,
-                config.n_head is not None,
-                config.n_embd is not None,
-            ]
-        )
-        assert type_given ^ params_given  # exactly one of these (XOR)
-        if type_given:
-            # translate from model_type to detailed configuration
-            config.merge_from_dict(
-                {
-                    # names follow the huggingface naming conventions
-                    # GPT-1
-                    "openai-gpt": dict(
-                        n_layer=12, n_head=12, n_embd=768
-                    ),  # 117M params
-                    # GPT-2 configs
-                    "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-                    "gpt2-medium": dict(
-                        n_layer=24, n_head=16, n_embd=1024
-                    ),  # 350M params
-                    "gpt2-large": dict(
-                        n_layer=36, n_head=20, n_embd=1280
-                    ),  # 774M params
-                    "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
-                    # Gophers
-                    "gopher-44m": dict(n_layer=8, n_head=16, n_embd=512),
-                    # (there are a number more...)
-                    # I made these tiny models up
-                    "gpt-mini": dict(n_layer=6, n_head=6, n_embd=192),
-                    "gpt-micro": dict(n_layer=4, n_head=4, n_embd=128),
-                    "gpt-nano": dict(n_layer=3, n_head=3, n_embd=48),
-                }[config.model_type]
-            )
-
         self.transformer = nn.ModuleDict(
             dict(
                 wte=nn.Embedding(config.vocab_size, config.n_embd),
@@ -206,9 +148,10 @@ class GPT(nn.Module):
                     p, mean=0.0, std=0.02 / math.sqrt(2 * config.n_layer)
                 )
 
-        # report number of parameters (note we don't count the decoder parameters in lm_head)
-        n_params = sum(p.numel() for p in self.transformer.parameters())
-        print("number of parameters: %.2fM" % (n_params / 1e6,))
+    @property
+    def num_parameters(self):
+        # note: we don't count the decoder parameters in lm_head
+        return sum(p.numel() for p in self.transformer.parameters())
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
@@ -220,52 +163,6 @@ class GPT(nn.Module):
         elif isinstance(module, nn.LayerNorm):
             torch.nn.init.zeros_(module.bias)
             torch.nn.init.ones_(module.weight)
-
-    @classmethod
-    def from_pretrained(cls, model_type):
-        """
-        Initialize a pretrained GPT model by copying over the weights
-        from a huggingface/transformers checkpoint.
-        """
-        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
-        from transformers import GPT2LMHeadModel
-
-        # create a from-scratch initialized minGPT model
-        config = cls.get_default_config()
-        config.model_type = model_type
-        config.vocab_size = 50257  # openai's model vocabulary
-        config.block_size = 1024  # openai's model block_size
-        model = GPT(config)
-        sd = model.state_dict()
-
-        # init a huggingface/transformers model
-        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-        sd_hf = model_hf.state_dict()
-
-        # copy while ensuring all of the parameters are aligned and match in names and shapes
-        keys = [k for k in sd_hf if not k.endswith("attn.masked_bias")]  # ignore these
-        transposed = [
-            "attn.c_attn.weight",
-            "attn.c_proj.weight",
-            "mlp.c_fc.weight",
-            "mlp.c_proj.weight",
-        ]
-        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla nn.Linear.
-        # this means that we have to transpose these weights when we import them
-        assert len(keys) == len(sd)
-        for k in keys:
-            if any(k.endswith(w) for w in transposed):
-                # special treatment for the Conv1D weights we need to transpose
-                assert sd_hf[k].shape[::-1] == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k].t())
-            else:
-                # vanilla copy over the other parameters
-                assert sd_hf[k].shape == sd[k].shape
-                with torch.no_grad():
-                    sd[k].copy_(sd_hf[k])
-
-        return model
 
     def configure_optimizers(self, train_config):
         """
